62f903596f4ca750fc26291039cc9b71521ea7b3
maha <maha@maha>
2022-08-20 05:30:53 +0530

    first sample program

--- mahalakshmi/src/maha/lakshmi.java
+++ mahalakshmi/src/maha/lakshmi.java
@@ -1,7 +1,10 @@
 package maha;
-public class lakshmi{
-	
-public static void main(String[] args) {
-System.out.println("Hello");    
+
+public class lakshmi {
+
+	public static void main(String[] args) {
+	System.out.println("HELLO FRIENDS");	// TODO Auto-generated method stub
+
 	}
+
 }
