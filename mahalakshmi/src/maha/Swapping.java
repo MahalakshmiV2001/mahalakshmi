f01277bc8a26ffad52974f5d3b046a2bd9db3b34
maha <maha@maha>
2022-08-26 09:24:14 +0530

    Variable initialization

---
+++ mahalakshmi/src/maha/Swapping.java
@@ -0,0 +1,17 @@
+package maha;
+
+public class Swapping {
+public static void main(String[] args) {
+	int n1=5, n2=21;
+	System.out.println("BEFORE SWAPPING");
+	System.out.println(n1);
+	System.out.println(n2);
+	int n3;
+	n3 = n1;
+	n1 = n2;
+	n2 = n3;
+	System.out.println("AFTER SWAPPING");
+	System.out.println(n1);
+	System.out.println(n2);
+}
+}
