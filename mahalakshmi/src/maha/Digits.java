3c33222cfed191fd9fba9c7a2830e59d82968f3d
maha <maha@maha>
2022-08-29 13:48:49 +0530

    split digits

---
+++ mahalakshmi/src/maha/Digits.java
@@ -0,0 +1,14 @@
+package maha;
+
+public class Digits {
+	public static void main(String args[]) {
+int num =328;
+int a,b,c ;
+a=num/100;
+b=(num%100)/10;
+c=(num%100)%10;
+System.out.println(a);
+System.out.println(b);
+System.out.println(c);
+}
+}
---
+++ mahalakshmi/src/maha/Digits2.java
@@ -0,0 +1,18 @@
+package maha;
+
+public class Digits2 {
+public static void main(String args[]) {
+int num =1126;
+int a,b,c,d;
+a=num/1000;
+b=(num%1000)/100;
+c=(num%100)/10;
+d=(num%10)%10;
+System.out.println(a);
+System.out.println(b);
+System.out.println(c);
+System.out.println(d);
+
+}
+
+}
