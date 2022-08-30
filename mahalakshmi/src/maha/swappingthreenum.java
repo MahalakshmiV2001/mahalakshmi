735c2f7141541e702a16f08ebb0bf3618aa134ce
maha <maha@maha>
2022-08-30 10:03:59 +0530

    Swapping the three numbers

--- mahalakshmi/src/maha/swappingthreenum.java
+++ mahalakshmi/src/maha/swappingthreenum.java
@@ -2,16 +2,23 @@ package maha;
 
 public class swappingthreenum {
 public static void main(String args[]) {
+	System.out.println("Before swapping");
 	int x=5;
 	int y=3;
 	int z=7;
+	System.out.println("x="+x);
+	System.out.println("y="+y);
+	System.out.println("z="+z);
 	x=x+y+z;
-	y=x-y+z;
+y=(x-y)-z;
+z=(x-y)-z;
+x=x-(y+z);
 	z=x-y+z;
 	x=x-y+z;
-	System.out.println(x);
-	System.out.println(y);
-	System.out.println(z);
+	System.out.println("After swapping");
+	System.out.println("x="+x);
+	System.out.println("y="+y);
+	System.out.println("z="+z);
 	
 
 	
