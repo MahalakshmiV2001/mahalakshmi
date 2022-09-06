5440c73a23080f6a17c77e52e69de594a61466fb
maha <maha@maha>
2022-09-06 08:35:38 +0530

    to find positive numbers

---
+++ mahalakshmi/src/maha/Bingbang.java
@@ -0,0 +1,19 @@
+package maha;
+
+public class Bingbang {
+public static void main(String args[]) {
+int n=5;
+if(n%5==0) {
+	System.out.println("Bang");
+} 
+if(n%3==0) {
+	System.out.println("Bing");
+}
+
+}
+}
+
+
+
+
+
