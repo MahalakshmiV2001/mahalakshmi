ea5a6888b9be17050cb80abf169de5d936c033e9
maha <maha@maha>
2022-09-08 09:07:02 +0530

    program to find the number which is divisible by 3 and 5

---
+++ mahalakshmi/src/maha/Numdivide.java
@@ -0,0 +1,17 @@
+package maha;
+
+public class Numdivide {
+	public static void main(String args[]){
+		
+		
+		//15 30 45
+	
+		for(int n=1;n<=50;n++) {
+		if(n%3==0 && n%5==0) 
+			System.out.println(n);
+		}
+
+		
+		}
+
+}
