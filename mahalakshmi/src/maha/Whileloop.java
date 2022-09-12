2d490812900a8171e8240ec3c324ac6cd19e571f
maha <maha@maha>
2022-09-12 21:48:46 +0530

    write a program using while loop

---
+++ mahalakshmi/src/maha/Whileloop.java
@@ -0,0 +1,14 @@
+package maha;
+
+public class Whileloop{
+public static void main(String args[]) {
+	int p=0;
+	for( int i=1; i<=5; i++)
+	{
+		p=p+i;
+		
+		 //System.out.println(p);
+	}
+	System.out.println(p);	
+}
+}
