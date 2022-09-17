26d6225e38fecc7f621ce21b6fef0f39d5746670
maha <maha@maha>
2022-09-17 08:50:39 +0530

    print the pattern using nested loop.

---
+++ mahalakshmi/src/maha/Patternnested.java
@@ -0,0 +1,15 @@
+package maha;
+
+public class Patternnested {
+public static void main(String args[]) {
+	int n=5;
+	for(int j=0;j<n;j++) {
+		for (int i=0;i<n;i++)
+		{
+			System.out.print("*");
+		}
+		System.out.println();
+	}
+		
+}
+}
