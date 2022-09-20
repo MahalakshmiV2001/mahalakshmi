fc84c9c9efe3d79a9c8ec7b2fc081eeed9e3b49e
maha <maha@maha>
2022-09-20 15:20:06 +0530

    pattern program

---
+++ ASSIGNMENT/src/practice/Whilejava.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class Whilejava {
+public static void main(String args[]) {        //Third question
+	int n=4;
+	for(int i=1;i<=n;i++) {
+		for(int j=4;j>4-i;j--) {
+			System.out.print(" * ");
+		}
+		System.out.println();
+	}
+	}
+
+}
