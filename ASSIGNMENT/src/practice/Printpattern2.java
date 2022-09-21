e0bf5caebd989976c287456aeb6db1b5945de47a
maha <maha@maha>
2022-09-21 09:47:24 +0530

    program to print some patterns

---
+++ ASSIGNMENT/src/practice/Printpattern2.java
@@ -0,0 +1,16 @@
+package practice;
+
+public class Printpattern2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+char i='A';
+for(int j=1;j<=4;j++) {
+	for(int k=1;k<=j;k++) {
+		System.out.print( i++ );
+	}
+	System.out.println();
+}
+	}
+
+}
